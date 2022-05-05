effb4b67a15cfb4e48988b8c563493b6d9eec92c
Umang Poudyal <umangpoudyal@gmail.com>
2022-05-05 18:28:38 -0400

    Structure

---
+++ src/parts/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './Card'
+
+function Home() {
+  return (
+    <div><Card /></div>
+  )
+}
+
+export default Home
---
+++ src/parts/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>Navbar
+        <image src="http://www.w3.org/2000/svg" />
+    </div>
+  )
+}
+
+export default Navbar
